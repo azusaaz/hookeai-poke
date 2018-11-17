7013cd0ccd26199b4d06b1c443fdc166a4eacdbb
“az” <shimazaki@aaa.email.ne.jp>
2018-11-17 08:26:24 -0500

    post checkout route added

--- routes/orders.js
+++ routes/orders.js
@@ -4,40 +4,16 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (knex) => {
-  // router.get("/", (req, res) => {
-  //   knex
-  //     .select("*")
-  //     .from("order")
-  //     .join('order_items', 'order.id', '=', 'order_items.order_id')
-  //     .join('menu', 'menu.id', '=', 'order_items.menu_id')
-  //     .then((results) => {
-  //       console.log(results);
-  //       res.json(results);
-  //   });
-  // });
-
   //past orders
-  // router.get("/:id", (req, res) => {
-  //   knex
-  //     .select("*")
-  //     .from("order")
-  //     .join('order_items', 'order.id', '=', 'order_items.order_id')
-  //     .join('users', 'users.id', '=', req.params.id)
-  //     .join('menu', 'menu.id', '=', 'order_items.menu_id')
-  //     .then((results) => {
-  //       console.log(results);
-  //       res.json(results);
-  //   });
-  // });
-
-  //name, phone number, cardinfo (# & ccv), userid, order items, order quantity
-
 
   router.post("/checkout", (req, res) => {
-    console.log("aaaa");
-    let tmpUserId = 1;
-    let tmpCart = [{
-      quatity: 2,
+
+    // *** After Merged ***
+    // Cart = req.body.cart;
+
+    // test obj
+    let Cart = [{
+      quantity: 2,
       menu_id: 1
     }, {
       quantity: 1,
@@ -47,58 +23,63 @@ module.exports = (knex) => {
       menu_id: 3
     }];
 
-    if (0) { // error
-
-    } else {
-
-      if (req.session["user_id"]) {
-
-
-      } else { // user didn't log-in
-
-        knex('users').insert({
-            name: 'test',
-            phone: 0000
-          })
+    knex.insert([{
+          name: req.body["email"],
+          phone: req.body["phone-number"]
+        }], "id").into('users')
+        .then((results) => {
+          return results[0];
+        })
+        .then((user_id) => 
+          knex.insert([{
+            card: req.body["card-num"],
+            expiry: req.body["ex-month"]+"/"+req.body["ex-year"],
+            ccv: req.body["cvv"],
+            user_id: user_id
+          }], "user_id").into('payment')
           .then((results) => {
-            console.log(result);
+            return results[0];
           })
+          .then((user_id) =>
+            knex.insert([{
+              user_id: user_id,
+            }], "id").into('order')
+          )
+          .then((results) => {
+            return results[0];
+          })
+          .then((order_id) => {  
+            Promise.all(Cart.map((item) => {
+              return knex.insert([{
+                order_id: order_id,
+                menu_id: item.menu_id,
+                quantity: item.quantity,
+        
+              }]).into('order_items')
+            }))
+          })
+        )
 
-        // knex('"order"').insert({
-        //     user_id: "ssss"
-        //   })
-        //   .then(
-
-        //     testObj.forEach(() => {
-
-        //       knex('order_items').insert({
-        //         quantity: "???",
-        //         menu_id: "???",
-        //         order_id: "???"
-        //       })
-
-        //     })
-        //   )
-
-      }
-    }
-    res.render('/confirmation')
+      res.redirect('/orders/confirmation');
   });
 
+
+  // select menu.name, order_items.quantity, menu.price from "order"
+  // join users on users.id = "order".user_id
+  // join order_items on "order".id = order_items.order_id
+  // join menu on menu.id = order_items.menu_id
+  // where users.id = 1;
+
   router.get("/checkout", (req, res) => {
-    console.log("aaa");
+
     res.render('checkout');
   });
 
   router.get("/confirmation", (req, res) => {
+
     res.render('confirmation');
   });
-
-  //name, phone number, cardinfo (# & ccv), userid, order items, order quantity
-  // router.post("/checkout", (req, res) => {
-
-  // });
-
+  
   router.get("/:id", (req, res) => {
     knex
       .select('menu.name', 'order_items.quantity', 'menu.price')
@@ -108,12 +89,14 @@ module.exports = (knex) => {
       .join('menu', 'menu.id', '=', 'order_items.menu_id')
       .where('users.id', '=', req.params.id)
       .then((result) => {
+
         const templateVars = {
           order: result
         }
         console.log(templateVars);
         res.render('pastOrders', templateVars);
       });
+
   });
 
   return router;
--- server.js
+++ server.js
@@ -17,6 +17,36 @@ const knexLogger  = require('knex-logger');
 // Seperated Routes for each Resource
 const menuRoutes = require("./routes/menu");
 const ordersRoutes = require("./routes/orders");
+// const usersRoutes = require("./routes/users");
+
+
+
+// Setup for Trillio
+const accountSid = 'ACca11b83c4d9f2a84ea589e3cabd69c68';
+const authToken = 'd9895972a326d1a84c4422af1c7d3fd8';
+
+const twilio = require('twilio');
+const client = new twilio(accountSid, authToken);
+
+let sendTexts = () => {
+client.messages.create({
+  //Send to resturant owner
+    body: 'So and so just ordered some food from you!  Get it ready!!',
+    to: '+16479902039', // Text this number
+    from: '+16474933577' // From a valid Twilio number
+  })
+  .then((message) => console.log(message.sid));
+
+client.messages.create({
+  //send to client
+    body: 'Your order from Hookeai Poke is ready for pickup!  Go to 416 Leslie St to pick up and enjoy.',
+    to: '+16479200506', // Text this number
+    from: '+16474933577' // From a valid Twilio number
+  })
+  .then((message) => console.log(message.sid));
+}
+//function to send texts with trillio
+// sendTexts()
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -40,6 +70,7 @@ app.use(express.static("public"));
 // Mount all resource routes
 app.use("/menu", menuRoutes(knex));
 app.use("/orders", ordersRoutes(knex));
+// app.use("/users", usersRoutes(knex));
 
 // Home page
 app.get("/", (req, res) => {
