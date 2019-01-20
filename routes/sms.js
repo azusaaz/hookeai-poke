12b1b12a67356353cee2b67d1544f0d851d1ac78
Az <shimazaki@aaa.email.ne.jp>
2019-01-20 01:58:50 +0000

    test

--- routes/sms.js
+++ routes/sms.js
@@ -6,10 +6,8 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-
-// off these two for demo
-// const accountSid = process.env.SID;
-// const authToken = process.env.AUTH;
+const accountSid = process.env.SID;
+const authToken = process.env.AUTH;
 
 const twilio = require('twilio');
 const client = new twilio(accountSid, authToken);
@@ -19,42 +17,45 @@ app.use(bodyParser.urlencoded({
 }));
 
 module.exports = (knex) => {
+
+  // comment out below for demo
+
   //Webhooks - Restaurant responds with either Ready or Confirmed, and correct response message
-  router.post('/', (req, res) => {
-    const twiml = new MessagingResponse();
-    knex
-      .select('phone')
-      .from("users")
-      .orderBy('id', 'DESC')
-      .limit(1)
-      .then((result) => {
-        const phoneNumber = JSON.stringify(result[0].phone);
-        if (req.body.Body == 'Ready') {
-          client.messages.create({
-              body: 'Your order from Hookeai Poke is ready for pickup!  Go to location: 416 Leslie St to pick up and enjoy.',
-              to: phoneNumber, // Text this number
-              from: process.env.PHONE_NUM // From a valid Twilio number
-            })
-            .then((message) => console.log(message.sid));
-        } else if (req.body.Body == 'Confirmed') {
-          client.messages.create({
-              body: 'Your order from Hookeai Poke has been recieved and being prepared!  You will be notified when ready.',
-              to: phoneNumber, // Text this number
-              from: process.env.PHONE_NUM // From a valid Twilio number
-            })
-            .then((message) => console.log(message.sid));
-        } else {
-          twiml.message(
-            'Derp, I dont understand what you said.  Please say either Confirmed or Ready.'
-          );
-        }
-      })
+  // router.post('/', (req, res) => {
+  //   const twiml = new MessagingResponse();
+  //   knex
+  //     .select('phone')
+  //     .from("users")
+  //     .orderBy('id', 'DESC')
+  //     .limit(1)
+  //     .then((result) => {
+  //       const phoneNumber = JSON.stringify(result[0].phone);
+  //       if (req.body.Body == 'Ready') {
+  //         client.messages.create({
+  //             body: 'Your order from Hookeai Poke is ready for pickup!  Go to location: 416 Leslie St to pick up and enjoy.',
+  //             to: phoneNumber, // Text this number
+  //             from: process.env.PHONE_NUM // From a valid Twilio number
+  //           })
+  //           .then((message) => console.log(message.sid));
+  //       } else if (req.body.Body == 'Confirmed') {
+  //         client.messages.create({
+  //             body: 'Your order from Hookeai Poke has been recieved and being prepared!  You will be notified when ready.',
+  //             to: phoneNumber, // Text this number
+  //             from: process.env.PHONE_NUM // From a valid Twilio number
+  //           })
+  //           .then((message) => console.log(message.sid));
+  //       } else {
+  //         twiml.message(
+  //           'Derp, I dont understand what you said.  Please say either Confirmed or Ready.'
+  //         );
+  //       }
+  //     })
 
-    res.writeHead(200, {
-      'Content-Type': 'text/xml'
-    });
-    res.end(twiml.toString());
-  });
+  //   res.writeHead(200, {
+  //     'Content-Type': 'text/xml'
+  //   });
+  //   res.end(twiml.toString());
+  // });
 
-  return router;
+  // return router;
 }
