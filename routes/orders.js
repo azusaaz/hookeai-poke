23a3e4a962366c4e729758e82bcf664700661e82
“az” <shimazaki@aaa.email.ne.jp>
2019-01-19 19:34:11 -0500

    change some info

--- routes/orders.js
+++ routes/orders.js
@@ -2,54 +2,51 @@
 const express = require('express');
 const router = express.Router();
 
-const accountSid = 'ACca11b83c4d9f2a84ea589e3cabd69c68';
-const authToken = 'd9895972a326d1a84c4422af1c7d3fd8';
+const accountSid = process.env.SID;
+const authToken = process.env.AUTH;
 
 const twilio = require('twilio');
 const client = new twilio(accountSid, authToken);
 
 module.exports = (knex) => {
 
-  router.post("/getItemInformation", (req, res) => {
-    console.log(req.body);
-    var fullItem = JSON.parse(req.body.itemIds)
-    var itemIds = Object.keys(fullItem)
+    router.post("/getItemInformation", (req, res) => {
+      var fullItem = JSON.parse(req.body.itemIds)
+      var itemIds = Object.keys(fullItem)
 
-    knex
-      .select('id', 'name', 'price')
-      .from('menu')
-      .then((results) => {
-        var filteredResults = results.filter((item) => {
-          return itemIds.indexOf(item.id.toString()) > -1;
+      knex
+        .select('id', 'name', 'price')
+        .from('menu')
+        .then((results) => {
+          var filteredResults = results.filter((item) => {
+            return itemIds.indexOf(item.id.toString()) > -1;
+          })
+
+          res.json(filteredResults);
         })
-        console.log(results);
+    });
 
-        res.json(filteredResults);
-      })
-  });
+    function getCartItems(items) {
+      let cart = [];
 
-  function getCartItems(items) {
-    let cart = [];
-
-    for (let key in items) {
-      if (key.match(/menu_id/)) {
-        let menu_id = key.replace(/menu_id/, "");
-        let tmp = {
-          menu_id: menu_id,
-          quantity: items[key]
-        };
-        cart.push(tmp);
+      for (let key in items) {
+        if (key.match(/menu_id/)) {
+          let menu_id = key.replace(/menu_id/, "");
+          let tmp = {
+            menu_id: menu_id,
+            quantity: items[key]
+          };
+          cart.push(tmp);
+        }
       }
+      return cart;
     }
-    return cart;
-  }
 
-  router.post("/checkout", (req, res) => {
+    router.post("/checkout", (req, res) => {
 
-    console.log(req.body);
-    let cart = getCartItems(req.body);
+      let cart = getCartItems(req.body);
 
-    knex.insert([{
+      knex.insert([{
           name: req.body["email"],
           phone: req.body["phone-number"]
         }], "id").into('users')
@@ -59,7 +56,7 @@ module.exports = (knex) => {
         .then((user_id) =>
           knex.insert([{
             card: req.body["card-num"],
-            expiry: req.body["ex-month"]+"/"+req.body["ex-year"],
+            expiry: req.body["ex-month"] + "/" + req.body["ex-year"],
             ccv: req.body["cvv"],
             user_id: user_id
           }], "user_id").into('payment')
@@ -86,54 +83,58 @@ module.exports = (knex) => {
           })
         )
 
-     res.redirect('/orders/confirmation');
-  });
+      res.redirect('/orders/confirmation');
+    });
 
-  router.get("/checkout", (req, res) => {
-     knex
-      .select('id', 'name', 'price')
-      .from('menu')
-      .then((results) => {
-        console.log(results);
-        const templateVars = {user: req.session.user_id,
-                              menuNames: results}
-        res.render('checkout', templateVars)
+    router.get("/checkout", (req, res) => {
+      knex
+        .select('id', 'name', 'price')
+        .from('menu')
+        .then((results) => {
+          const templateVars = {
+            user: req.session.user_id,
+            menuNames: results
+          }
+          res.render('checkout', templateVars)
+        });
+
+    });
+
+    router.get("/confirmation", (req, res) => {
+        client.messages.create({
+            //Send to resturant owner
+            body: 'Poke bowls have been ordered!  Get it ready!!',
+            to: process.env.PHONE_NUM_OWNER, // Text this number
+            from: process.env.PHONE_NUM // From a valid Twilio number
+          })
+          .then((message) =>
+            // console.log(message.sid));
+
+            const templateVars = {
+              user: req.session.user_id,
+              phone: req.body["phone-number"]
+            }
+            res.render('confirmation', templateVars);
+          });
+
+      router.get("/:id", (req, res) => {
+        knex
+          .select('menu.name', 'order_items.quantity', 'menu.price')
+          .from('order')
+          .join('users', 'users.id', '=', 'order.user_id')
+          .join('order_items', 'order.id', '=', 'order_items.order_id')
+          .join('menu', 'menu.id', '=', 'order_items.menu_id')
+          .where('users.id', '=', req.params.id)
+          .then((result) => {
+
+            const templateVars = {
+              order: result,
+              user: req.session.user_id
+            }
+
+            res.render('pastOrders', templateVars);
+          });
+
       });
-
-  });
-
-  router.get("/confirmation", (req, res) => {
-    client.messages.create({
-        //Send to resturant owner
-        body: 'Poke bowls have been ordered!  Get it ready!!',
-        to: '+16479200506', // Text this number
-        from: '+16474933577' // From a valid Twilio number
-      })
-      .then((message) => console.log(message.sid));
-
-    const templateVars = {
-      user: req.session.user_id,
-      phone: req.body["phone-number"]
-    }
-    res.render('confirmation', templateVars);
-  });
-
-  router.get("/:id", (req, res) => {
-    knex
-      .select('menu.name', 'order_items.quantity', 'menu.price')
-      .from('order')
-      .join('users', 'users.id', '=', 'order.user_id')
-      .join('order_items', 'order.id', '=', 'order_items.order_id')
-      .join('menu', 'menu.id', '=', 'order_items.menu_id')
-      .where('users.id', '=', req.params.id)
-      .then((result) => {
-
-        const templateVars = {order: result, user: req.session.user_id}
-
-        console.log(templateVars);
-        res.render('pastOrders', templateVars);
-      });
-
-  });
-  return router;
-};
+      return router;
+    };
--- routes/sms.js
+++ routes/sms.js
@@ -7,8 +7,9 @@ const bodyParser = require("body-parser");
 const app = express();
 
 
-const accountSid = 'ACca11b83c4d9f2a84ea589e3cabd69c68';
-const authToken = 'd9895972a326d1a84c4422af1c7d3fd8';
+// off these two for demo
+// const accountSid = process.env.SID;
+// const authToken = process.env.AUTH;
 
 const twilio = require('twilio');
 const client = new twilio(accountSid, authToken);
@@ -18,42 +19,42 @@ app.use(bodyParser.urlencoded({
 }));
 
 module.exports = (knex) => {
-//Webhooks - Restaurant responds with either Ready or Confirmed, and correct response message
-router.post('/', (req, res) => {
-  const twiml = new MessagingResponse();
-  knex
-    .select('phone')
-    .from("users")
-    .orderBy('id', 'DESC')
-    .limit(1)
-    .then((result) => {
-      const phoneNumber = JSON.stringify(result[0].phone);
-      if (req.body.Body == 'Ready') {
-        client.messages.create({
-            body: 'Your order from Hookeai Poke is ready for pickup!  Go to location: 416 Leslie St to pick up and enjoy.',
-            to: phoneNumber, // Text this number
-            from: '+16474933577' // From a valid Twilio number
-          })
-          .then((message) => console.log(message.sid));
-      } else if (req.body.Body == 'Confirmed') {
-        client.messages.create({
-            body: 'Your order from Hookeai Poke has been recieved and being prepared!  You will be notified when ready.',
-            to: phoneNumber, // Text this number
-            from: '+16474933577' // From a valid Twilio number
-          })
-          .then((message) => console.log(message.sid));
-      } else {
-        twiml.message(
-          'Derp, I dont understand what you said.  Please say either Confirmed or Ready.'
-        );
-      }
-    })
+  //Webhooks - Restaurant responds with either Ready or Confirmed, and correct response message
+  router.post('/', (req, res) => {
+    const twiml = new MessagingResponse();
+    knex
+      .select('phone')
+      .from("users")
+      .orderBy('id', 'DESC')
+      .limit(1)
+      .then((result) => {
+        const phoneNumber = JSON.stringify(result[0].phone);
+        if (req.body.Body == 'Ready') {
+          client.messages.create({
+              body: 'Your order from Hookeai Poke is ready for pickup!  Go to location: 416 Leslie St to pick up and enjoy.',
+              to: phoneNumber, // Text this number
+              from: process.env.PHONE_NUM // From a valid Twilio number
+            })
+            .then((message) => console.log(message.sid));
+        } else if (req.body.Body == 'Confirmed') {
+          client.messages.create({
+              body: 'Your order from Hookeai Poke has been recieved and being prepared!  You will be notified when ready.',
+              to: phoneNumber, // Text this number
+              from: process.env.PHONE_NUM // From a valid Twilio number
+            })
+            .then((message) => console.log(message.sid));
+        } else {
+          twiml.message(
+            'Derp, I dont understand what you said.  Please say either Confirmed or Ready.'
+          );
+        }
+      })
 
-  res.writeHead(200, {
-    'Content-Type': 'text/xml'
+    res.writeHead(200, {
+      'Content-Type': 'text/xml'
+    });
+    res.end(twiml.toString());
   });
-  res.end(twiml.toString());
-});
 
   return router;
 }
